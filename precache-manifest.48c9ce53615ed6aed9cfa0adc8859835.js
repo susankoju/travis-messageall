7f2d8f2339e23f35235a26253b410f7d2cd43011
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-11 06:44:19 +0000

    Deploy susankoju/travis-messageall to github.com/susankoju/travis-messageall.git:gh-pages

---
+++ precache-manifest.48c9ce53615ed6aed9cfa0adc8859835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e5d45164ac68563126be9988139ef7",
+    "url": "/travis-messageall/index.html"
+  },
+  {
+    "revision": "52dcddd5d88f47835525",
+    "url": "/travis-messageall/static/css/main.e5419d69.chunk.css"
+  },
+  {
+    "revision": "6f12f5849cdb77ad09ce",
+    "url": "/travis-messageall/static/js/2.b9fd2ffa.chunk.js"
+  },
+  {
+    "revision": "138a6d51240ab070e8d1b20c54be6394",
+    "url": "/travis-messageall/static/js/2.b9fd2ffa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52dcddd5d88f47835525",
+    "url": "/travis-messageall/static/js/main.78557fea.chunk.js"
+  },
+  {
+    "revision": "4c673c3eda71c15f9062",
+    "url": "/travis-messageall/static/js/runtime-main.43f216b2.js"
+  }
+]);
