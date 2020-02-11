6abcac3d13a6cdc591c325a49c7fa44fc7e16197
susankoju <aksusankoju@gmail.com>
2020-02-11 10:00:55 +0545

    test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getElementById("root");
   expect(linkElement).toBeInTheDocument();
 });
